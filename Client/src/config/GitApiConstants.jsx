874f75e73c247b15c915922cc7e05486b89ac659
Chandra Mudili <chmudili@microsoft.com>
2020-07-30 12:41:40 +0530

    Update GitApiConstants.jsx

--- Client/src/config/GitApiConstants.jsx
+++ Client/src/config/GitApiConstants.jsx
@@ -1,7 +1,7 @@
 const { Octokit } = require("@octokit/rest");
-//const gittoken = "bc0d9337c4714764557ce5bddc4983d4e7bd9f7f";
+//const gittoken = "**";
 //export const gituser = "anukgupt";
-const gittoken = "ecb0684f8eef466046b9496965d5f90ee8234b84";
+const gittoken = "**";
 export const gituser = "chandrasekharmudili";
 
 export const octokit = new Octokit({
